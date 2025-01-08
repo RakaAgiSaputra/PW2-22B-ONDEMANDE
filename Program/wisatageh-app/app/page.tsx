0bae6fe9c7999fd3c46501f8c684772e29732258
master <Raka_Agi_Saputra@teknokrat.ac.id>
2025-01-08 21:29:51 +0700

    raka: test app/page.tsx

--- Program/wisatageh-app/app/page.tsx
+++ Program/wisatageh-app/app/page.tsx
@@ -1,9 +1,14 @@
+import Navbar from "@/components/ui/halaman_utama/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <>
-   <h1>Test</h1>
+   {/* <Navbar/> */}
+
+
+
+
    </>
   );
 }
