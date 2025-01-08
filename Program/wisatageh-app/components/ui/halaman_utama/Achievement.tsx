ab000dbc5e3bb9432c986b00f310bcb87cdaa274
ZaVyn57 <agitzayn57@gmail.com>
2025-01-09 00:12:56 +0700

    aqgit: create achievement section

--- Program/wisatageh-app/components/ui/halaman_utama/Achievement.tsx
+++ Program/wisatageh-app/components/ui/halaman_utama/Achievement.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Achievement() {
   return (
     <section className="bg-primaryColor py-20 text-center">
-      <div className="flex flex-col md:flex-row justify-evenly items-center text-black space-y-6 md:space-y-0">
+      <div className="flex flex-col md:flex-row justify-evenly items-center text-black space-y-6 md:space-y-0 select-none">
         <div>
           <h1 className="text-7xl font-bold">32+</h1>
           <p className="text-lg font-bold">Tujuan Perjalanan</p>
@@ -17,7 +17,7 @@ export default function Achievement() {
           <p className="text-lg font-bold">Ulasan Diterima</p>
         </div>
         <div className="text-left">
-          <h2 className="text-2xl font-extrabold">
+          <h2 className="text-2xl font-extrabold select-none">
             Jelajahi Perjalanan <br />
             Petualangan <br />
             Bersama WisataGeh
