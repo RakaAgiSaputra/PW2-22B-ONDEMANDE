2f45f5b3a3f13202bee3ee1a107aba0ef6269abc
master <Raka_Agi_Saputra@teknokrat.ac.id>
2025-01-15 18:27:15 +0700

    raka: create wishlist

---
+++ Program/wisatageh-app/app/wishlist/page.tsx
@@ -0,0 +1,12 @@
+export default function WishListPage(){
+    const batas = "px-20 pt-5 pb-5";
+    return(
+        <>
+        <div className={batas}>
+            <h1>Sdfasfd</h1>
+
+        </div>
+        
+        </>
+    )
+}
