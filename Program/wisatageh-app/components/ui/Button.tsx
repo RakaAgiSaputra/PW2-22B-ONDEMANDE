73d212a8becf84f4843aea464c129c2813b4a8e0
master <zanitekno@gmail.com>
2025-01-15 11:52:28 +0700

    zani: create ButtonProps

---
+++ Program/wisatageh-app/components/ui/Button.tsx
@@ -0,0 +1,15 @@
+interface PropsPage {
+  words: string;
+  bg: string;
+  ukuran: string;
+}
+
+export function ButtonProps(
+    props: PropsPage
+) {
+  return (
+    <button className={`${props.ukuran} font-bold py-6 px-6 border-primaryColor border-2 rounded-lg ${props.bg}`}>
+      {props.words}
+    </button>
+  );
+}
