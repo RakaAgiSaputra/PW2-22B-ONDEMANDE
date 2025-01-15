bdc59bdddb03b65ec4b27962c28ecd5761d98739
master <zanitekno@gmail.com>
2025-01-15 14:09:28 +0700

    zani: add footer

---
+++ Program/wisatageh-app/components/halaman_utama/Footer.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+export function Footer() {
+  return (
+    <footer>
+      <section className="flex flew-row gap-6 h-1/4 bg-primaryColor px-20 pt-20 pb-10 justify-between items-start">
+        <div className="w-1/4 flex flex-col">
+          <Link href="" className="hover:text-gray-300 pb-3 text-xl font-bold">
+            WisataGeh
+          </Link>
+          <Link href="" className="hover:text-gray-300 py-3">
+            Temukan keindahan mempesona dan budaya Lampung yang semarak di
+            Indonesia, dengan WisataGeh. Petualangan Anda menanti!
+          </Link>
+          <Link href="" className="hover:text-gray-300 py-3">
+            © Copyright 2025, All Rights Reserved
+          </Link>
+        </div>
+        <div className="flex flex-col">
+          <Link href="" className="hover:text-gray-300 font-bold">
+            Company
+          </Link>
+          <Link href="">Destination</Link>
+        </div>
+        <div className="flex flex-col">
+          <Link href="" className="hover:text-gray-300 font-bold">
+            Quick Links
+          </Link>
+          <Link href="">Works</Link>
+          <Link href="">Testimonial</Link>
+        </div>
+        <div className="flex flex-col">
+          <Link href="" className="hover:text-gray-300 font-bold">
+            Help
+          </Link>
+          <Link href="">Customer Support</Link>
+          <Link href="">Terms & Conditions</Link>
+          <Link href="">Privacy Policy</Link>
+        </div>
+      </section>
+    </footer>
+  );
+}
